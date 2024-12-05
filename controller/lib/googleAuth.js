1d6193fcd2be7eba1a04ac011d186e4824bb40c4
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-05 16:41:55 +0300

    create function to get refresh token

---
+++ controller/lib/googleAuth.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+function getNewLoginUrl() {
+  const url = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&scope=${process.env.GOOGLE_SCOPE}&response_type=code&access_type=offline`;
+  return axios.get(url);
+}
+
+function getRefreshToken(code) {
+  const url = `https://oauth2.googleapis.com/token`;
+
+  const params = {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    code,
+    redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+    grant_type: "authorization_code",
+  };
+
+  return axios.post(url, null, {
+    params,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+}
