10a9483b0be1eb4e1e7591a169267daeb6d8c802
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-05 16:42:18 +0300

    create function to get access token

--- controller/lib/googleAuth.js
+++ controller/lib/googleAuth.js
@@ -23,3 +23,23 @@ function getRefreshToken(code) {
     },
   });
 }
+
+function getAccessToken(refreshToken) {
+  const url = `https://oauth2.googleapis.com/token`;
+
+  const params = {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    refresh_token: refreshToken,
+    grant_type: "refresh_token",
+  };
+
+  return axios.post(url, null, {
+    params,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded", 
+    },
+  });
+}
+
+module.exports = { getNewLoginUrl, getRefreshToken, getAccessToken };
