c13db77bdbb2b1de7927bb826ff629bd6e56b7df
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-05 23:22:29 +0300

    created postgresql db connection

---
+++ controller/lib/dbHandler.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+// Test the connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Error acquiring client", err.stack);
+  } else {
+    console.log("Connected to the database");
+  }
+  release(); // Release the client back to the pool
+});
+
