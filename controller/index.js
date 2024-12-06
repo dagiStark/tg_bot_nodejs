302fdf6771ee6725805e368e8a0e19e513250b72
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-06 18:29:51 +0300

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -6,7 +6,12 @@ const {
   getAccessToken,
 } = require("./lib/googleAuth");
 const { getLimitedMedia } = require("./lib/googlePhotos");
-const { updateRefreshTokenInDb, getRefreshTokenFromDb } = require("./lib/dbHandler");
+const {
+  updateRefreshTokenInDb,
+  getRefreshTokenFromDb,
+  batchWriteItems,
+  getRandomPhoto,
+} = require("./lib/dbHandler");
 
 const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
 const accessToken = process.env.GOOGLE_ACCESS_TOKEN;
@@ -14,15 +19,30 @@ const accessToken = process.env.GOOGLE_ACCESS_TOKEN;
 async function handler(req, method) {
   try {
     if (method === "GET") {
-      if(req.url === "/pg-get-token") {
+      // TO TEST
+      if (req.url === "/get-random") {
+        const url = await getRandomPhoto();
+        return url;
+      }
+
+      // TO TEST
+      if (req.url === "/batch") {
+        const testingData = ["url1, url2, url3"];
+        await batchWriteItems(testingData);
+        return "Success";
+      }
+
+      if (req.url === "/pg-get-token") {
         const refreshTokenFromDb = await getRefreshTokenFromDb();
         return refreshTokenFromDb.rows[0].value;
       }
+
       if (req.url === "/pg-update") {
         const tokenToUpdate = "SomeNewValue";
         await updateRefreshTokenInDb(tokenToUpdate);
         return "Success";
       }
+
       if (req.url === "/test") {
         const data = await getNewLoginUrl();
         return data.config.url;
@@ -42,7 +62,8 @@ async function handler(req, method) {
         const data = req.query.code;
         const result = await getRefreshToken(data);
         const refreshToken = result.data.refresh_token;
-        return refreshToken;
+        await updateRefreshTokenInDb(refreshToken);
+        return "Successfully updated refresh token";
       }
       return "unknown request";
     }
